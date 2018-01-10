b6f0c91b717538d369dd311a1152a63400cf7c11
Vladmir Orlov <vorlov@bbtv.com>
2018-01-09 20:09:56 -0800

    Initial Commit

---
+++ check_user_age/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  check_user_age
+//
+//  Created by Valdimir Orlov on 2018-01-09.
+//  Copyright © 2018 Valdimir Orlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ check_user_ageTests/check_user_ageTests.swift
@@ -0,0 +1,36 @@
+//
+//  check_user_ageTests.swift
+//  check_user_ageTests
+//
+//  Created by Valdimir Orlov on 2018-01-09.
+//  Copyright © 2018 Valdimir Orlov. All rights reserved.
+//
+
+import XCTest
+@testable import check_user_age
+
+class check_user_ageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
