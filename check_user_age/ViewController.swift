d1b127f4681234f6a5c993d6a3d20a1dc592d98f
Vladmir Orlov <volodia.orlov@gmail.com>
2018-01-12 16:34:02 -0800

    My first project in Xcode & first commit. Check user age app.

--- check_user_age/ViewController.swift
+++ check_user_age/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var bbtvImage: UIImageView!
+    
+    @IBOutlet weak var message: UILabel!
+    
+    @IBOutlet weak var ageInput: UITextField!
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        
+        let num:Int! = Int(ageInput.text!)
+        
+        print(num)
+        
+        if num == nil || num < 0 || num > 120 {
+            message.text = "The age should be between 0 and 120"
+        }else{
+            if num < 21 {
+                message.text = "You can not use this app"
+            }else{
+                message.text = "Welcom to the app!"
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bbtvImage.image = UIImage(named: "BBTV_Logo")
     }
 
     override func didReceiveMemoryWarning() {
